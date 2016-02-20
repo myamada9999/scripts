40e2a73ffac67b842a782471d5a9b2ffaaf4e12f
Masahiro Yamada <masahiro9999@gmail.com>
2016-02-20 20:21:37 +0900

    setup-git.sh: Add command check.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

--- setup-git.sh
+++ setup-git.sh
@@ -2,7 +2,19 @@
 
 MAIL_ADDRESS="masahiro9999@gmail.com"
 
+function abort
+{
+	echo "ERROR: $@" 1>&2
+	exit 1
+}
+
+# Check git, vim command
+[ ! `which git` ] && abort "git command not found."
+[ ! `which vim` ] && abort "vim command not found."
+
 git config --global push.default matching
 git config --global user.name 'Masahiro Yamada'
 git config --global user.email $MAIL_ADDRESS
 git config --global core.editor 'vim'
+
+exit 0
