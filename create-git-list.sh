00a9fb61c65ed5bc392feaccbcfa1e67ea89089b
Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>
2016-03-04 12:21:59 +0000

    Add create-git-list.sh.
    Signed-off-by: Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>

---
+++ create-git-list.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+GIT_DIR=~/git-repositories
+DIRS=`ls $GIT_DIR`
+GIT_LIST="git-list.txt"
+CURRENT_DIR=`pwd`
+
+function abort
+{
+	echo "ERROR: $@" 1>&2
+	exit 1
+}
+
+function warn
+{
+	echo "ERROR: $@" 1>&2
+}
+
+cd $GIT_DIR
+for dir in `echo $DIRS`
+do
+    if [ ! -d $dir ]; then
+        warn "$dir is not directory"
+        continue
+    fi
+    if [ ! -d $dir/.git ]; then
+        warn "$dir is not git directory"
+        continue
+    fi
+    URL=`cat $dir/.git/config | grep url | cut -d"=" -f2`
+    echo $URL >> $CURRENT_DIR/$GIT_LIST
+done
+cd $CURRENT_DIR
+
+exit 0
