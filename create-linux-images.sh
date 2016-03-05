738975ad9367d694bcc2083d0e34f927bfd26eaa
Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>
2016-03-05 21:00:20 +0900

    Add create-linux-image.sh
    Signed-off-by: Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>

---
+++ create-linux-images.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CURRENT_DIR=`pwd`
+WORK_DIR=`mktemp -d`
+ARCH="x86"
+CROSS_COMPILE_ENVIRONMENT=""
+CROSS_COMPILE=""
+
+function abort
+{
+	echo "ERROR: $@" 1>&2
+	exit 1
+}
+
+function warn
+{
+	echo "ERROR: $@" 1>&2
+}
+
+[ -f ./Makefile ] || abort "Makefile is not found."
+[ -f ./.config ] || abort ".config is not found."
+[ -d $WORK_DIR ] || abort "$WORK_DIR is not found."
+mkdir $WORK_DIR/boot
+mkdir $WORK_DIR/lib
+mkdir $WORK_DIR/lib/modules
+
+source $CROSS_COMPILE_ENVIRONMENT
+ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE make menuconfig || abort "Menuconfig failed"
+ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE make -j8 || abort "Compile failed"
+ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE sudo make install INSTALL_PATH=$WORK_DIR/boot || abort  "Install failed"
+ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE make modules_install INSTALL_MOD_PATH=$WORK_DIR || abort "Module install failed"
+
+echo "Images was installed to $WORK_DIR"
+
+exit 0
