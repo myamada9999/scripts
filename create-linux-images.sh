1941ced72dfe83a4bb8d0322a9d7ff40ab1524ea
Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>
2016-03-16 23:10:13 +0000

    create-linux-images.sh: Clean file and add comments.
    Signed-off-by: Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>

--- create-linux-images.sh
+++ create-linux-images.sh
@@ -7,20 +7,6 @@ CROSS_COMPILE_ENVIRONMENT=""
 CROSS_COMPILE=""
 OUTPUT_DIR=""
 
-while getopts "d:" flag; do
-    case $flag in
-        \?) OPT_ERROR=1; break;;
-        d) OUTPUT_DIR="$OPTARG";;
-    esac
-done
-
-shift $(( $OPTIND - 1 ))
-
-if [ $OPT_ERROR ]; then
-    echo >&2 "usage: $0 [-d output_directory] "
-    exit 1
-fi
-
 function abort
 {
 	echo "ERROR: $@" 1>&2
@@ -32,13 +18,23 @@ function warn
 	echo "ERROR: $@" 1>&2
 }
 
+while getopts "d:" flag; do
+    case $flag in
+        \?) OPT_ERROR=1; break;;
+        d) OUTPUT_DIR="$OPTARG";;
+    esac
+done
+shift $(( $OPTIND - 1 ))
+[ $OPT_ERROR ] && abort "usage: $0 [-d output_directory]"
+
 [ -f ./Makefile ] || abort "Makefile is not found."
 [ -f ./.config ] || abort ".config is not found."
 [ -d $WORK_DIR ] || abort "$WORK_DIR is not found."
 if [ ! -z $OUTPUT_DIR ]; then
- [ -d $OUTPUT_DIR ] || abort "i$OUTPUT_DIR is not found."
+ [ -d $OUTPUT_DIR ] || abort "$OUTPUT_DIR is not found."
 fi
 
+# Get kernel version
 VERSION=`cat Makefile | grep "^VERSION = " | cut -d "=" -f 2`
 PATCHLEVEL=`cat Makefile | grep "^PATCHLEVEL = " | cut -d "=" -f 2`
 SUBLEVEL=`cat Makefile | grep "^SUBLEVEL = " | cut -d "=" -f 2`
@@ -50,22 +46,16 @@ VERSION=`echo $VERSION`
 PATCHLEVEL=`echo $PATCHLEVEL`
 SUBLEVEL=`echo $SUBLEVEL`
 EXTRAVERSION=`echo $EXTRAVERSION`
-if [ ! -z $LOCALVERSION ]; then
-    LOCALVERSION=`echo $LOCALVERSION`
-fi
+[ ! -z $LOCALVERSION ] && LOCALVERSION=`echo $LOCALVERSION`
 
 KERNEL_VERSION=$VERSION"."$PATCHLEVEL"."$SUBLEVEL$EXTRAVERSION
-if [ ! -z $LOCALVERSION ]; then
-    KERNEL_VERSION=$KERNEL_VERSION$LOCALVERSION
-fi
-
-echo $KERNEL_VERSION
-exit 0
+[ ! -z $LOCALVERSION ] && KERNEL_VERSION=$KERNEL_VERSION$LOCALVERSION
 
 mkdir $WORK_DIR/boot
 mkdir $WORK_DIR/lib
 mkdir $WORK_DIR/lib/modules
 
+# run make and make install
 if [ ! -z $CROSS_COMPILE_ENVIRONMENT ]; then
     sudo bash -c " \
                   source $CROSS_COMPILE_ENVIRONMENT; \
@@ -83,6 +73,7 @@ else
                  "
 fi
 
+# Rename a temporary directory to output directory
 if [ -z $OUTPUT_DIR ]; then
     [ -d "/tmp/"$KERNLE_VERSION".old" ] && sudo rm -rf "/tmp/"$KERNEL_VERSION".old"
     [ -d "/tmp/"$KERNLE_VERSION ] && sudo mv "/tmp/"$KERNEL_VERSION "/tmp/"$KERNEL_VERSION".old"
