adb69f534a380c14908084ce06cb711e0532ded9
Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>
2016-03-17 00:24:20 +0000

    setup-for-develop.sh: Add trash-cli package.
    Signed-off-by: Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>

--- setup-for-develop.sh
+++ setup-for-develop.sh
@@ -3,4 +3,4 @@
 # This script set up developer environment for Debina/Ubuntu.
 
 # Essentials
-sudo apt-get install -y gcc build-essential autoconf automake libtool tmux vim libncurses-dev
+sudo apt-get install -y gcc build-essential autoconf automake libtool tmux vim libncurses-dev trash-cli
