bff8d5293e2b0f57699d11f2ba82e5d4bfcf5a19
Masahiro Yamada <masahiro9999@gmail.com>
2016-03-20 20:24:28 +0900

    git-hub-clone.sh: Add new script.
    Signed-off-by: Masahiro Yamada <masahiro9999@gmail.com>

---
+++ git-hub-clone.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function usage
+{
+    echo "Usage: $0 USER REPOSITORYY"
+	rm -rf $WORK_DIR
+    exit 1
+}
+
+[ $# != 2 ] && usage
+USER=$1
+REPOSITORY=$2
+
+git clone https://github.com/$1/$2
+
+exit 0
