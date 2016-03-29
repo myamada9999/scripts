3183638bf655bdecd1467fa9d76015c8d5eb0c6f
Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>
2016-03-29 06:00:33 +0000

    nohup-bitbake.sh: Fix small bug.
    Signed-off-by: Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>

--- nohup-bitbake.sh
+++ nohup-bitbake.sh
@@ -14,7 +14,7 @@ while getopts "bmt" flag; do
         \?) OPT_ERROR=1; break;;
         b) CORE_IMAGE="core-image-base";;
         m) CORE_IMAGE="core-image-minimal";;
-        t) TOOLCHAIN=1;;
+        t) TOOLCHAIN="meta-toolchain";;
     esac
 done
 shift $(( $OPTIND - 1 ))
@@ -23,10 +23,10 @@ shift $(( $OPTIND - 1 ))
 [ `which send-mail.sh` ] || abort "send-mail.sh is not found."
 nohup bash -c "bitbake $CORE_IMAGE"
 mv nohup.out $CORE_IMAGE.log
-if [ $TOOLCHAIN ]; then
-    nohup bash -c "bitbake meta-toolchain"
-    mv nohup.out $CORE_IMAGE.log
+if [ ! -z $TOOLCHAIN ]; then
+    nohup bash -c "bitbake $TOOLCHAIN"
+    mv nohup.out $TOOLCHAIN.log
 fi
-send-mail.sh "myamada9999@gmail.com" "bitbake finished." "`tail -n 50 nohup.out`"
+send-mail.sh "@mail address" "bitbake finished." "`tail -n 50 $CORE_IMAGE.log`"
 
 exit 0
